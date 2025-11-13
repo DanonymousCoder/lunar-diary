7e16215d18c54ebe6c8acd3bae9bfdb5d1219a15
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2025-11-13 15:49:57 +0100

    feat: update landing and dashboard components with new styles and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import {Moon, Sparkles} from 'lucide-react';
-import {Link} from 'react-router-dom';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Dashboard from './dashboard';
+import { Link, Routes, Route } from 'react-router-dom';
+import Dashboard from './dashboard.jsx';
+
+
+function LandingPage() {
+    return (
+      <div className='container'>
+
+          <main>
+            <Moon size={80} className='moon' />
+            <h1>Lunar Diary</h1>
+            <p>Your emotions, written in the star.</p>
+            <Link to='/dashboard' className='landing-btn'>Enter Diary <Sparkles /></Link>
+          </main>
+        
+      </div>
+    );
+};
 
 function LunarDiary() {
   return (
-    <BrowserRouter>
       <Routes>
-        <Route>
-          <Route index element={<LandingPage />} />
-          <Route path='dashboard' element={<Dashboard />} />
-        </Route>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Moon } from 'lucide-react';
+import { Link } from 'react-router-dom';
 import './dashboard.css';
 
 function Dashboard() {
     return (
-        <div className='container'>
+        <div className='container-dashboard'>
+            <nav>
+                <div className='logo'>
+                    <Moon size={50} className='dash-moon' />
+                    <h1>Lunar Diary</h1>
+                </div>
 
+                <div className='nav-links'>
+                    <Link></Link>
+                    <Link></Link>
+                </div>
+            </nav>
+
+            <main>
+
+            </main>
         </div>
     )
 }
